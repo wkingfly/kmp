77f2c84b7a86f9cf44d9a76711781f95c3ddb356
wkingfly <wkingfly@gmail.com>
2014-08-06 19:27:32 +0800

    kmp algorithm

---
+++ kmp/kmp.py
@@ -0,0 +1,56 @@
+import pdb
+
+class strFind:
+    def __init__(self, child, matrix):
+        self.child = list(child)
+        self.matrix = list(matrix)
+        self.part_num = []
+        self.part_list = []
+
+    def part_match(self):
+        matrix_len = len(self.matrix)
+        child_len = len(self.child)
+        matrix_part = []
+        i = 0
+        if self.part_list:
+            return
+        while i < matrix_len:
+            i += 1
+            matrix_part.append(self.child[0:i])
+
+        for i in matrix_part:
+            prefix = []
+            postfix = []
+            j = 1
+            while j < len(i):
+                prefix.append(''.join(i[0:j]))
+                postfix.append(''.join(i[j:len(i)]))
+                j += 1
+            pre = set(prefix)
+            post = set(postfix)
+            self.part_num.append(len(''.join(pre & post)))
+        j = 0
+        for i in self.child:
+            self.part_list.append(self.part_num[j])    
+            j += 1
+
+    def find(self):
+        self.part_match()
+        j = 0
+        equals = 0
+        for i in self.matrix:
+            if i == self.child[j]:
+                equals += 1
+                j += 1
+                if equals == 6:
+                    pdb.set_trace()
+                if equals == len(self.child):
+                    return True
+            else:
+                if equals:
+                    move_num = equals - self.part_list[equals-1]
+                else:
+                    move_num = 1
+                self.matrix = self.matrix[move_num:len(self.matrix)]
+                self.find()
+        return False
---
+++ test/test_kmp.py
@@ -0,0 +1,25 @@
+import unittest
+import sys
+sys.path.append('..')
+from kmp import kmp
+
+class Test_strFind(unittest.TestCase):
+
+    def setUp(self):
+        matrix = 'iacdabcabcdabcabcix' 
+        child = 'abcabcx'
+        self.sF = kmp.strFind(child, matrix)
+    def tearDown(self):
+        del self.sF
+    
+    def test_part_match(self):
+        self.sF.part_match()
+        l = [0,0,0,1,2,3,0]
+        self.assertEqual(self.sF.part_list, l)
+
+    def test_find(self):
+        self.assertFalse(self.sF.find())
+
+
+if __name__ == '__main__':
+    unittest.main()
